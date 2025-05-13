ae55d2d1df2c7b209f305fb8f2f83d84288e68d5
AswinMWeb <133523889+AswinMWeb@users.noreply.github.com>
2025-05-13 20:46:49 +0000

    Restored to 'bf0d4de21cf63aa702344fd09070f78910a8ea76'
    Replit-Restored-To: bf0d4de21cf63aa702344fd09070f78910a8ea76

--- index.php
+++ index.php
@@ -1,24 +1,25 @@
-
 <?php
 session_start();
-
-if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] != 1) {
-    header('Location: /login.php');
-    exit();
-}
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 <title>Aswin</title>
-</head>
-<body>
+  </head>
+     <body>
+
     <h1>Assignment 1</h1>
-    <p>Welcome, <?=$_SESSION['username']?></p>
-    <p>Today is <?=date('l, F j, Y')?></p>
-    <footer>
+
+
+       
+
+        <p> Welcome , <?=$_SESSION['username']?> </p>
+     
+
+    </body>
+
+   <footer>
         <p><a href="/logout.php">Logout</a></p>
     </footer>
-</body>
+      
 </html>
-```
--- login.php
+++ login.php
@@ -1,28 +1,23 @@
 
-<?php
-session_start();
-if (isset($_SESSION['authenticated']) && $_SESSION['authenticated'] == 1) {
-    header('Location: /index.php');
-    exit();
-}
-?>
 <!DOCTYPE html>
 <html>
 <head>
 <title>Login</title>
-</head>
-<body>
+  </head>
+     <body>
+
     <h1>Login Form</h1>
-    <form action="/validate.php" method="post">
-        <label for="username">Username:</label>
-        <br>
-        <input type="text" id="username" name="username">
-        <br> 
-        <label for="password">Password:</label>
-        <br>
-        <input type="password" id="password" name="password">
-        <br><br>
-        <input type="submit" value="Submit">
-    </form>
-</body>
-</html>
+
+
+    </body>
+<form action="/validate.php" method ="post">
+  <label for="username">Username:</label>
+  <br>
+  <input type="text" id="username" name="username" >
+  <br> 
+  <label for="password"></label>Password:</label>
+  <br>
+  <input type="password" id="password" name="password" >
+  <br><br>
+  <input type="submit" value="Submit">
+</form>
