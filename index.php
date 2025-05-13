88a8105760401d724772d717ed7c766977ab1a65
AswinMWeb <133523889+AswinMWeb@users.noreply.github.com>
2025-05-13 14:47:50 +0000

    updated index page

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
+<!DOCTYPE html>
 <html>
-  <head>
-    <title>PHP Test</title>
+<head>
+<title>Aswin</title>
   </head>
-  <body>
-    <?php echo '<p>Hello World</p>'; ?> 
+     <body>
 
+    <h1>My First Heading</h1>
+     
+
+    </body>
 </html>
